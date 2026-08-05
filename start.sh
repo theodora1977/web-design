f4e027e992f5e762c94bbdc4ae059e05a59c0898
dev <dev@example.com>
2026-08-05 16:13:04 +0100

    added for the deployment

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-python -m uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+exec gunicorn app.main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:${PORT:-8000}
